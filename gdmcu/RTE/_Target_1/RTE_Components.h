75a1c7ac98b9b2e1bb9698e28427bd824e600a20
jamesxu <xxj8711@gmail.com>
2024-06-24 18:17:45 +0800

    create directory: gd mcu

---
+++ gdmcu/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,56 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'gd32' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "gd32f10x.h"
+
+/* GigaDevice::Device:GD32F10x_StdPeripherals:FWDGT@2.3.0 */
+#define RTE_DEVICE_STDPERIPHERALS_FWDGT
+/* GigaDevice::Device:GD32F10x_StdPeripherals:GPIO@2.3.0 */
+#define RTE_DEVICE_STDPERIPHERALS_GPIO
+/* Keil::Compiler&ARM Compiler:Event Recorder&DAP@1.5.1 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+/* Keil::Compiler&ARM Compiler:I/O:File&File System@1.2.0 */
+#define RTE_Compiler_IO_File            /* Compiler I/O: File */
+          #define RTE_Compiler_IO_File_FS         /* Compiler I/O: File (File System) */
+/* Keil::Compiler&ARM Compiler:I/O:STDERR&Breakpoint@1.2.0 */
+#define RTE_Compiler_IO_STDERR          /* Compiler I/O: STDERR */
+          #define RTE_Compiler_IO_STDERR_BKPT     /* Compiler I/O: STDERR Breakpoint */
+/* Keil::Compiler&ARM Compiler:I/O:STDIN&Breakpoint@1.2.0 */
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+          #define RTE_Compiler_IO_STDIN_BKPT      /* Compiler I/O: STDIN Breakpoint */
+/* Keil::Compiler&ARM Compiler:I/O:STDOUT&Breakpoint@1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_BKPT     /* Compiler I/O: STDOUT Breakpoint */
+/* Keil::Compiler&ARM Compiler:I/O:TTY&Breakpoint@1.2.0 */
+#define RTE_Compiler_IO_TTY             /* Compiler I/O: TTY */
+          #define RTE_Compiler_IO_TTY_BKPT        /* Compiler I/O: TTY Breakpoint */
+/* Keil::Network&MDK-Plus:CORE&IPv4 Release@7.19.0 */
+#define RTE_Network_Core                /* Network Core */
+          #define RTE_Network_IPv4                /* Network IPv4 Stack */
+          #define RTE_Network_Release             /* Network Release Version */
+/* Keil::Network&MDK-Plus:Legacy API@7.19.0 */
+#define RTE_Network_Legacy              /* Network Legacy */
+/* Keil::Network&MDK-Plus:Service:DNS Client@7.19.0 */
+#define RTE_Network_DNS_Client          /* Network DNS Client */
+/* Keil::Network&MDK-Plus:Service:Web Server Compact&HTTP@7.19.0 */
+#define RTE_Network_Web_Server_RO       /* Network Web Server with Read-only Web Resources */
+/* Keil::USB&MDK-Plus:CORE&Release@6.17.0 */
+#define RTE_USB_Core                    /* USB Core */
+          #define RTE_USB_Core_Release            /* USB Core Release Version */
+
+
+#endif /* RTE_COMPONENTS_H */
